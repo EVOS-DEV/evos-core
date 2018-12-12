76c1139be69a52da4767d02237b796122682fb9f
EVOS-DEV <43036359+EVOS-DEV@users.noreply.github.com>
2018-12-12 00:26:09 -0500

    fixed bug with checking vin for collateral during transition period

--- src/obfuscation.cpp
+++ src/obfuscation.cpp
@@ -2118,7 +2118,7 @@ bool CObfuScationSigner::IsVinAssociatedWithPubkey(CTxIn& vin, CPubKey& pubkey)
     uint256 hash;
     if (GetTransaction(vin.prevout.hash, txVin, hash, true)) {
         BOOST_FOREACH (CTxOut out, txVin.vout) {
-            if (out.nValue == mnActivateAmount && out.nValue == mnActivateSurvive) {
+            if (out.nValue == mnActivateAmount || out.nValue == mnActivateSurvive) {
                 if (out.scriptPubKey == payee2) return true;
             }
         }
